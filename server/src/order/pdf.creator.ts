d23298cd78c640572f643ecca20ffeb0fe56c751
Roman <roma.oliynyk83@gmail.com>
2023-06-19 23:53:22 +0300

    install puppeteer :)

--- server/src/order/pdf.creator.ts
+++ server/src/order/pdf.creator.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@nestjs/common';
-// import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer';
 import { Observable, from } from 'rxjs';
-
-import * as wkhtmltopdf from 'wkhtmltopdf';
 
 @Injectable()
 export class PdfCreator {
@@ -10,36 +8,16 @@ export class PdfCreator {
     return from(this.crate_pdf(html));
   }
 
-  private async crate_pdf(html: string) {
+  private async crate_pdf(html: string, templateName?: string) {
     try {
-      return new Promise<Buffer>((resolve, reject) => {
-        wkhtmltopdf(html, {}, (error: any, stream: any) => {
-          if (error) {
-            reject(error);
-          } else {
-            const chunks: Buffer[] = [];
-            stream.on('data', (chunk: Buffer) => chunks.push(chunk));
-            stream.on('end', () => resolve(Buffer.concat(chunks)));
-            stream.on('error', (error: any) => reject(error));
-          }
-        });
-      });
-    } catch (error) {
-      console.error('Ошибка при создании PDF:', error);
-      throw error;
+      const browser = await puppeteer.launch({ headless: true });
+      const page = await browser.newPage();
+      await page.setContent(html);
+      const pdf = await page.pdf({ format: 'A4' });
+      await browser.close();
+      return pdf;
+    } catch (e) {
+      throw e;
     }
   }
-
-  // private async crate_pdf(html: string, templateName?: string) {
-  //   try {
-  //     const browser = await puppeteer.launch({ headless: true });
-  //     const page = await browser.newPage();
-  //     await page.setContent(html);
-  //     const pdf = await page.pdf({ format: 'A4' });
-  //     await browser.close();
-  //     return pdf;
-  //   } catch (e) {
-  //     throw e;
-  //   }
-  // }
 }
