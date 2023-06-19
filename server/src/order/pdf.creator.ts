74b7f0db190c4a63a5e5f805b8fe798628d3ca96
Roman <roma.oliynyk83@gmail.com>
2023-06-19 22:01:32 +0300

    Replacing puppeteer with html-pdf

--- server/src/order/pdf.creator.ts
+++ server/src/order/pdf.creator.ts
@@ -1,23 +1,43 @@
 import { Injectable } from '@nestjs/common';
-import puppeteer from 'puppeteer';
+// import puppeteer from 'puppeteer';
 import { Observable, from } from 'rxjs';
+import * as pdf from 'html-pdf';
 
 @Injectable()
 export class PdfCreator {
   get(html: string): Observable<Buffer> {
-    return from(this.crate_pdf(html));
+    return this.crate_pdf(html);
   }
 
-  private async crate_pdf(html: string, templateName?: string) {
-    try {
-      const browser = await puppeteer.launch({ headless: true });
-      const page = await browser.newPage();
-      await page.setContent(html);
-      const pdf = await page.pdf({ format: 'A4' });
-      await browser.close();
-      return pdf;
-    } catch (e) {
-      throw e;
-    }
+  private crate_pdf(html: string) {
+    return new Observable<Buffer>((observer) => {
+      pdf
+        .create(html, {
+          format: 'A4',
+        })
+        .toBuffer((err, buffer) => {
+          if (err) {
+            observer.error(err);
+          } else {
+            console.log('buffer', buffer);
+
+            observer.next(buffer);
+            observer.complete();
+          }
+        });
+    });
   }
+
+  // private async crate_pdf(html: string, templateName?: string) {
+  //   try {
+  //     const browser = await puppeteer.launch({ headless: true });
+  //     const page = await browser.newPage();
+  //     await page.setContent(html);
+  //     const pdf = await page.pdf({ format: 'A4' });
+  //     await browser.close();
+  //     return pdf;
+  //   } catch (e) {
+  //     throw e;
+  //   }
+  // }
 }
