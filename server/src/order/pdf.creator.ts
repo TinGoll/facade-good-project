052dc506c103a67598679eaa8262bab1da09f766
Roman <roma.oliynyk83@gmail.com>
2023-06-25 12:58:27 +0300

    fix: pdf styles

--- server/src/order/pdf.creator.ts
+++ server/src/order/pdf.creator.ts
@@ -11,16 +11,17 @@ export class PdfCreator {
   private async crate_pdf(html: string, templateName?: string) {
     try {
       const browser = await puppeteer.launch({
-        devtools:false,
+        devtools: false,
         headless: true,
         args: ['--no-sandbox', '--single-process'],
         executablePath: '/usr/bin/chromium-browser',
         // executablePath: '/usr/bin/chromium',
         // executablePath: '/usr/bin/google-chrome',
-
       });
       const page = await browser.newPage();
-      await page.setContent(html);
+      await page.goto('data:text/html;charset=UTF-8,' + encodeURIComponent(html), {
+        waitUntil: 'networkidle0',
+      });
       const pdf = await page.pdf({ format: 'A4' });
       await browser.close();
       return pdf;
