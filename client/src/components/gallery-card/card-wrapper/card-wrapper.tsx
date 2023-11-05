cbeb46777331554dd407d7becf46177eae33c8b0
Roman <roma.oliynyk83@gmail.com>
2023-11-05 18:35:39 +0300

    n3: add card wrapper

---
+++ client/src/components/gallery-card/card-wrapper/card-wrapper.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+const CardWrapper: React.FC<Props> = () => {
+  return <div>CardWrapper</div>;
+};
+
+export default CardWrapper;
--- client/src/pages/order.tsx
+++ client/src/pages/order.tsx
@@ -1,24 +1,15 @@
 import { HeadFC, PageProps } from "gatsby";
 import React from "react";
 import { FacadeGood } from "../app-types";
-import {
-  Main,
-  Container,
-  HeadText,
-  HeadTextWrapper,
-} from "../components/facade-good/facade-good";
+import { Main, Container } from "../components/facade-good/facade-good";
 import SiteFooter from "../components/footer/site-footer";
 import SiteHeadder from "../components/header/site-header";
 import { SEO } from "../components/seo";
 import AppLayout from "../layout/app-layout";
 import ScrollToTop from "../components/scroll/scroll-to-top";
-import OrderForm from "../components/order-form/order-form";
-import { OrderFormProvider } from "../components/order-form/order-form-provider";
+
 
 const OrderPage: React.FC<PageProps> = ({ location, params }) => {
-  const params1 = new URLSearchParams(location.search);
-  const parameter1 = params1.get("test");
-
   return (
     <AppLayout>
       <ScrollToTop />
@@ -33,12 +24,7 @@ const OrderPage: React.FC<PageProps> = ({ location, params }) => {
             paddingTop: 100,
           })}
         >
-          <HeadTextWrapper>
-            <HeadText>Форма для заказа мебельных фасадов.</HeadText>
-          </HeadTextWrapper>
-          <OrderFormProvider>
-            <OrderForm />
-          </OrderFormProvider>
+
         </Container>
       </Main>
       <SiteFooter />
@@ -49,40 +35,3 @@ const OrderPage: React.FC<PageProps> = ({ location, params }) => {
 export default OrderPage;
 
 export const Head: HeadFC = () => <SEO title="Facade Good - Сделать заказ" />;
-
-/**
- *  <Select
-            outline
-            css={{ marginTop: 40, marginBottom: 40 }}
-            value={selectValue[0]}
-            onChange={(opt) => selectValue[1](opt)}
-            options={massiv}
-            placeholder="Массив"
-          />
-          <Select
-            outline
-            value={selectValue2[0]}
-            onChange={(opt) => selectValue2[1](opt)}
-            options={options}
-            placeholder="Массив"
-          />
-          <Select
-            outline
-            value={selectValue3[0]}
-            onChange={(opt) => selectValue3[1](opt)}
-            options={options}
-            placeholder="Третий"
-          />
-          <Checkbox checked={false} label="Сверлить" />
-          <Textbox />
-          <Grid columns={4} gap={16}>
-            <Item>Item 1</Item>
-            <Item>Item 2</Item>
-            <Item>Item 3</Item>
-            <Item>Item 4</Item>
-            <Item>Item 5</Item>
-            <Item>Item 6</Item>
-            <Item>Item 7</Item>
-            <Item>Item 8</Item>
-            </Grid>
- */
